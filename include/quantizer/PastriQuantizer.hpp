8dac8fd8fa5b78bc4a924c510fa403531d2e0751
Xin Liang <xin@Xins-MacBook-Pro-2.local>
2021-03-01 01:10:44 -0600

    embedded encoding opt 1: use block-wise exponent

--- include/quantizer/PastriQuantizer.hpp
+++ include/quantizer/PastriQuantizer.hpp
@@ -103,30 +103,35 @@ namespace SZ {
             size_t n = this->unpred.size();
             if(n == 0) return;
             const T * data = this->unpred.data();
-            T max_val = 0;
-            for(int i=0; i<n; i++){
-                if(fabs(data[i]) > max_val){
-                    max_val = fabs(data[i]);
-                }
-            }
-            int data_exp = 0;
-            frexp(max_val, &data_exp);
-            const int num_bitplanes = data_exp - eb_exp;
-            // std::cout << data_exp << " " << eb_exp << ", num_bitplanes = " << num_bitplanes << std::endl;
+
             const T * data_pos = data;
             const int block_size = 32;
+            const int num_blocks = (n - 1) / block_size + 1;
             uint64_t int_data_buffer[block_size];
-            unsigned char * encoded_sign_pos = c;
-            unsigned char * encoded_data_pos = c + (((n - 1) / block_size + 1) * (block_size / 8));
-            // std::cout << "encoded_sign_pos = " << encoded_data_pos - c << std::endl;
-            // encode exponent
-            *reinterpret_cast<int *>(encoded_data_pos) = num_bitplanes;
-            encoded_data_pos += sizeof(int);
+            unsigned char * encoded_num_bitplanes_pos = c;
+            unsigned char * encoded_sign_pos = encoded_num_bitplanes_pos + num_blocks;
+            unsigned char * encoded_data_pos = encoded_sign_pos + (((n - 1) / block_size + 1) * (block_size / 8));
             uint32_t * encoded_int_sign_pos = reinterpret_cast<uint32_t*>(encoded_sign_pos);
             uint32_t * encoded_int_data_pos = reinterpret_cast<uint32_t*>(encoded_data_pos);
             int i = 0;
             if(n >= block_size){
                 for(; i<n - block_size; i+=block_size){
+                    T max_val = 0;
+                    for(int j=0; j<block_size; j++){
+                        if(fabs(data_pos[j]) > max_val){
+                            max_val = fabs(data_pos[j]);
+                        }
+                    }
+                    int data_exp = 0;
+                    frexp(max_val, &data_exp);
+                    int num_bitplanes = data_exp - eb_exp;
+                    if(num_bitplanes <= 0){
+                        *(encoded_num_bitplanes_pos ++) = 0;
+                        continue;
+                    }
+                    else{
+                        *(encoded_num_bitplanes_pos ++) = (unsigned char) num_bitplanes;
+                    }
                     uint32_t sign_bitplane = 0;
                     for(int j=0; j<block_size; j++){
                         T cur_data = *(data_pos++);
@@ -149,22 +154,38 @@ namespace SZ {
             // rest
             if(i != n){
                 int rest_size = n - i;
-                uint32_t sign_bitplane = 0;
+                T max_val = 0;
                 for(int j=0; j<rest_size; j++){
-                    T cur_data = *(data_pos++);
-                    T shifted_data = ldexp(cur_data, num_bitplanes - data_exp);
-                    int64_t fix_point = (int64_t) shifted_data;
-                    uint32_t sign = cur_data < 0;
-                    int_data_buffer[j] = sign ? -fix_point : +fix_point;
-                    sign_bitplane += sign << j;
-                }
-                *(encoded_int_sign_pos ++) = sign_bitplane;
-                for(int k=num_bitplanes - 1; k>=0; k--){
-                    uint32_t bitplane_value = 0;
-                    for (int j=0; j<rest_size; j++){
-                        bitplane_value += (uint32_t)((int_data_buffer[j] >> k) & 1u) << j;
+                    if(fabs(data_pos[j]) > max_val){
+                        max_val = fabs(data_pos[j]);
                     }
-                    *(encoded_int_data_pos ++) = bitplane_value;
+                }
+                int data_exp = 0;
+                frexp(max_val, &data_exp);
+                int num_bitplanes = data_exp - eb_exp;
+                if(num_bitplanes <= 0){
+                    *(encoded_num_bitplanes_pos ++) = 0;
+                }
+                else{
+                    *(encoded_num_bitplanes_pos ++) = (unsigned char) num_bitplanes;
+
+                    uint32_t sign_bitplane = 0;
+                    for(int j=0; j<rest_size; j++){
+                        T cur_data = *(data_pos++);
+                        T shifted_data = ldexp(cur_data, num_bitplanes - data_exp);
+                        int64_t fix_point = (int64_t) shifted_data;
+                        uint32_t sign = cur_data < 0;
+                        int_data_buffer[j] = sign ? -fix_point : +fix_point;
+                        sign_bitplane += sign << j;
+                    }
+                    *(encoded_int_sign_pos ++) = sign_bitplane;
+                    for(int k=num_bitplanes - 1; k>=0; k--){
+                        uint32_t bitplane_value = 0;
+                        for (int j=0; j<rest_size; j++){
+                            bitplane_value += (uint32_t)((int_data_buffer[j] >> k) & 1u) << j;
+                        }
+                        *(encoded_int_data_pos ++) = bitplane_value;
+                    }
                 }
             }
             c = reinterpret_cast<unsigned char*>(encoded_int_data_pos);
@@ -175,45 +196,50 @@ namespace SZ {
             size_t n = unpred_size;
             T * data_pos = data;
             const int block_size = 32;
+            const int num_blocks = (n - 1) / block_size + 1;
             uint64_t int_data_buffer[block_size];
-            unsigned char const * encoded_sign_pos = c;
-            unsigned char const * encoded_data_pos = c + (((n - 1) / block_size + 1) * (block_size / 8));
-            // decode exponent
-            int num_bitplanes = *reinterpret_cast<int const*>(encoded_data_pos);
-            encoded_data_pos += sizeof(int);
+            unsigned char const * encoded_num_bitplanes_pos = c;
+            unsigned char const * encoded_sign_pos = encoded_num_bitplanes_pos + num_blocks;
+            unsigned char const * encoded_data_pos = encoded_sign_pos + (((n - 1) / block_size + 1) * (block_size / 8));
             uint32_t const * encoded_int_sign_pos = reinterpret_cast<uint32_t const*>(encoded_sign_pos);
             uint32_t const * encoded_int_data_pos = reinterpret_cast<uint32_t const*>(encoded_data_pos);
             int i = 0;
             if(n >= block_size){
                 for(; i<n - block_size; i+=block_size){
-                    memset(int_data_buffer, 0, block_size * sizeof(uint64_t));
-                    for(int k=num_bitplanes - 1; k>=0; k--){
-                        uint32_t bitplane_value = *(encoded_int_data_pos ++);
-                        for (int j=0; j<block_size; j++, bitplane_value >>= 1){
-                            int_data_buffer[j] += (uint64_t)(bitplane_value & 1u) << k;
+                    int num_bitplanes = *(encoded_num_bitplanes_pos ++);
+                    if(num_bitplanes){
+                        memset(int_data_buffer, 0, block_size * sizeof(uint64_t));
+                        for(int k=num_bitplanes - 1; k>=0; k--){
+                            uint32_t bitplane_value = *(encoded_int_data_pos ++);
+                            for (int j=0; j<block_size; j++, bitplane_value >>= 1){
+                                int_data_buffer[j] += (uint64_t)(bitplane_value & 1u) << k;
+                            }
                         }
-                    }
-                    uint32_t sign_bitplane = *(encoded_int_sign_pos ++);
-                    for(int j=0; j<block_size; j++, sign_bitplane >>= 1){
-                        T cur_data = ldexp((T)int_data_buffer[j], eb_exp);
-                        *(data_pos++) = (sign_bitplane & 1u) ? -cur_data : cur_data;
+                        uint32_t sign_bitplane = *(encoded_int_sign_pos ++);
+                        for(int j=0; j<block_size; j++, sign_bitplane >>= 1){
+                            T cur_data = ldexp((T)int_data_buffer[j], eb_exp);
+                            *(data_pos++) = (sign_bitplane & 1u) ? -cur_data : cur_data;
+                        }                        
                     }
                 }
             }
             // rest
             if(i != n){
-                int rest_size = n - i;
-                memset(int_data_buffer, 0, rest_size * sizeof(uint64_t));
-                for(int k=num_bitplanes - 1; k>=0; k--){
-                    uint32_t bitplane_value = *(encoded_int_data_pos ++);
-                    for (int j=0; j<rest_size; j++, bitplane_value >>= 1){
-                        int_data_buffer[j] += (uint64_t)(bitplane_value & 1u) << k;
+                int num_bitplanes = *(encoded_num_bitplanes_pos ++);
+                if(num_bitplanes){
+                    int rest_size = n - i;
+                    memset(int_data_buffer, 0, rest_size * sizeof(uint64_t));
+                    for(int k=num_bitplanes - 1; k>=0; k--){
+                        uint32_t bitplane_value = *(encoded_int_data_pos ++);
+                        for (int j=0; j<rest_size; j++, bitplane_value >>= 1){
+                            int_data_buffer[j] += (uint64_t)(bitplane_value & 1u) << k;
+                        }
                     }
-                }
-                uint32_t sign_bitplane = *(encoded_int_sign_pos ++);
-                for(int j=0; j<rest_size; j++, sign_bitplane >>= 1){
-                    T cur_data = ldexp((T)int_data_buffer[j], eb_exp);
-                    *(data_pos++) = (sign_bitplane & 1u) ? -cur_data : cur_data;
+                    uint32_t sign_bitplane = *(encoded_int_sign_pos ++);
+                    for(int j=0; j<rest_size; j++, sign_bitplane >>= 1){
+                        T cur_data = ldexp((T)int_data_buffer[j], eb_exp);
+                        *(data_pos++) = (sign_bitplane & 1u) ? -cur_data : cur_data;
+                    }                    
                 }
             }
             c = reinterpret_cast<unsigned char const*>(encoded_int_data_pos);
