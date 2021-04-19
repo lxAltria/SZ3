25e75f2a0661039fa187539fb7e0c89ababeeb91
Xin Liang <xin@Xins-MacBook-Pro-2.local>
2021-04-19 12:34:26 -0500

    update fast encoder type 2

--- include/encoder/FastEncoder.hpp
+++ include/encoder/FastEncoder.hpp
@@ -3,71 +3,72 @@
 
 namespace SZ {
 
+    class BitEncoder{
+    public:
+        BitEncoder(uint64_t * stream_begin_pos){
+            stream_begin = stream_begin_pos;
+            stream_pos = stream_begin;
+            buffer = 0;
+            position = 0;
+        }
+        inline void encode(uint64_t b){
+            buffer |= b << position;
+            position ++;
+            if(position == 64){
+                *(stream_pos ++) = buffer;
+                buffer = 0;
+                position = 0;
+            }
+        }
+        void flush(){
+            if(position){
+                *(stream_pos ++) = buffer;
+                buffer = 0;
+                position = 0;
+            }
+        }
+        uint32_t size(){
+            return (stream_pos - stream_begin);
+        }
+    private:
+        uint64_t buffer = 0;
+        uint8_t position = 0;
+        uint64_t * stream_pos = NULL;
+        uint64_t * stream_begin = NULL;
+    };
+
+    class BitDecoder{
+    public:
+        BitDecoder(uint64_t const * stream_begin_pos){
+            stream_begin = stream_begin_pos;
+            stream_pos = stream_begin;
+            buffer = 0;
+            position = 0;
+        }
+        uint64_t decode(){
+            if(position == 0){
+                buffer = *(stream_pos ++);
+                position = 64;
+            }
+            uint64_t b = buffer & 1u;
+            buffer >>= 1;
+            position --;
+            return b;
+        }
+        uint32_t size(){
+            return (stream_pos - stream_begin);
+        }
+    private:
+        uint64_t buffer = 0;
+        uint8_t position = 0;
+        uint64_t const * stream_pos = NULL;
+        uint64_t const * stream_begin = NULL;
+    };
+
+
     template<class T>
     class FastEncoder : public concepts::EncoderInterface<T> {
         public:
-            class BitEncoder{
-            public:
-                BitEncoder(uint64_t * stream_begin_pos){
-                    stream_begin = stream_begin_pos;
-                    stream_pos = stream_begin;
-                    buffer = 0;
-                    position = 0;
-                }
-                void encode(uint64_t b){
-                    buffer += b << position;
-                    position ++;
-                    if(position == 64){
-                        *(stream_pos ++) = buffer;
-                        buffer = 0;
-                        position = 0;
-                    }
-                }
-                void flush(){
-                    if(position){
-                        *(stream_pos ++) = buffer;
-                        buffer = 0;
-                        position = 0;
-                    }
-                }
-                uint32_t size(){
-                    return (stream_pos - stream_begin);
-                }
-            private:
-                uint64_t buffer = 0;
-                uint8_t position = 0;
-                uint64_t * stream_pos = NULL;
-                uint64_t * stream_begin = NULL;
-            };
-
-            class BitDecoder{
-            public:
-                BitDecoder(uint64_t const * stream_begin_pos){
-                    stream_begin = stream_begin_pos;
-                    stream_pos = stream_begin;
-                    buffer = 0;
-                    position = 0;
-                }
-                uint64_t decode(){
-                    if(position == 0){
-                        buffer = *(stream_pos ++);
-                        position = 64;
-                    }
-                    uint64_t b = buffer & 1u;
-                    buffer >>= 1;
-                    position --;
-                    return b;
-                }
-                uint32_t size(){
-                    return (stream_pos - stream_begin);
-                }
-            private:
-                uint64_t buffer = 0;
-                uint8_t position = 0;
-                uint64_t const * stream_pos = NULL;
-                uint64_t const * stream_begin = NULL;
-            };
-
             FastEncoder(){};
             void preprocess_encode(const std::vector<T> &bins, int stateNum){}
 
@@ -149,6 +150,45 @@ namespace SZ {
             uint save(uchar *&c){ return 0;}
 
             void load(const uchar *&c, size_t &remaining_length){}
+    };
+    template<class T>
+    class FastEncoderType2 : public FastEncoder<T>{
+        public:
+            FastEncoderType2(){};
+            size_t encode(const std::vector<T> &bins, uchar *&bytes){
+                BitEncoder encoder(reinterpret_cast<uint64_t*>(bytes));
+                for(int i=0; i<bins.size(); i++){
+                    switch(bins[i]){
+                        // fixed Huffman encoding:
+                        // 0->'0'
+                        // 1->'1'
+                        case 0:{
+                            encoder.encode(0);
+                            break;                            
+                        }
+                        case 1:{
+                            encoder.encode(1);
+                            break;                            
+                        }
+                        default:{
+                            printf("bins out of range\n");
+                            exit(0);
+                        }
+                    }
+                }
+                encoder.flush();
+                size_t size = encoder.size() * sizeof(uint64_t);
+                bytes += size;
+                return size;
+            }
+            std::vector<T> decode(const uchar *&bytes, size_t targetLength){
+                std::vector<T> bins(targetLength);
+                BitDecoder decorder(reinterpret_cast<const uint64_t*>(bytes));
+                for(int i=0; i<targetLength; i++){
+                    bins[i] = decorder.decode();
+                }
+                return bins;
+            }
     };
 }
 #endif
